9f581f915800ebc84665de6346a46be6787ac1e4
Your Name <your-email@example.com>
2025-08-27 21:58:57 +0300

    removed database.py

--- backend/app/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-Base = declarative_base()
-engine = create_engine('sqlite:///plants.db')
-Session = sessionmaker(bind=engine)
-session = Session()
