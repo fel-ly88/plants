874fbfb84a3b91bcdcb03b5f560d696849d23261
Your Name <your-email@example.com>
2025-08-27 22:02:54 +0300

    removed app.py

--- backend/app/app.py
+++
@@ -1,8 +0,0 @@
-from utils import generate_id, current_timestamp, validate_name, format_response
-
-
-new_id = generate_id()
-print("Generated ID:", new_id)
-
-print("Timestamp:", current_timestamp())
-print("Valid name?", validate_name("Rose"))
